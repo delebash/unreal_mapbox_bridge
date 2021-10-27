f90fe7905de00b5ca8ea4fa941f110925ba98381
Daniel Elebash <delebash@users.noreply.github.com>
2021-10-27 13:24:15 -0400

    code comments

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// app.use((req, res, next) => {
-//     res.append('Cross-Origin-Resource-Policy', 'same-origin');
-//     res.append('Cross-Origin-Embedder-Policy', 'require-corp"');
-//     res.append('Cross-Origin-Opener-Policy', 'same-origin');
-//     next();
-// });
+;
 app.use(express.static(('dist'),
     {
         setHeaders: (res) => {
+            // Required for SharedBufferArray to work which is necessary for wasm-vips
             res.setHeader('Cross-Origin-Resource-Policy', 'same-origin')
             res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
             res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+            //
         }
     }
 ))
-// Setting up the public directory
-
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
