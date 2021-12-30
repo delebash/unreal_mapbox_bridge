997e758198f57dac1407420177a90105c269afe4
daniel <danelebash@hotmail.com>
2021-12-30 18:48:09 -0500

    try to fix failed to resolve entry for package vue

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {VitePWA} from 'vite-plugin-pwa'
 import mkcert from'vite-plugin-mkcert'
+import path from 'path'
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        },
+    },
     server: {
         // https: true  //hotmodule reload does not work if https is enabled
     },
