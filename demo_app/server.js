2512ebf698b6633a111e0cd51431de10080fa0b7
daniel <danelebash@hotmail.com>
2022-01-01 18:51:17 -0500

    add demo app

---
+++ demo_app/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(('dist'),
+    {
+        setHeaders: (res) => {
+            // Required for SharedBufferArray to work which is necessary for wasm-vips
+            res.setHeader('Cross-Origin-Resource-Policy', 'same-origin')
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+            //
+        }
+    }
+))
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
+
