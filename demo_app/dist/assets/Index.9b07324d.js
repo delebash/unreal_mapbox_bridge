9a89a4219440342466a9cbac34335daded65218a
daniel <danelebash@hotmail.com>
2022-01-26 03:47:01 -0500

    update dist

---
+++ demo_app/dist/assets/Index.9b07324d.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.d457f111.js";import{d as n,r as a,c as r,o as s}from"./vendor.0af61b56.js";const t=n({name:"PageIndex"});function c(p,f,_,d,m,x){const e=a("q-page");return s(),r(e,{class:"flex flex-center"})}var u=o(t,[["render",c]]);export{u as default};
