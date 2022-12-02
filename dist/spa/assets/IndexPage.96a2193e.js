0419584dca496350cdf1a8b3be4e44daf5f88ed7
Daniel Elebash <delebash@users.noreply.github.com>
2022-12-02 12:22:39 -0500

    fix Default download no scaling

---
+++ dist/spa/assets/IndexPage.96a2193e.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.3878cac8.js";import{aj as a,aY as t,ak as n,aU as o,an as s,ap as r}from"./index.7fe13973.js";const c=t({name:"IndexPage"}),l=r("img",{alt:"Unreal Vault Organizer logo",style:{width:"200px",height:"200px"}},null,-1);function p(i,d,f,_,x,m){return n(),o(e,{class:"flex flex-center"},{default:s(()=>[l]),_:1})}var h=a(c,[["render",p]]);export{h as default};
