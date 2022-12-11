188de6ee58fb9075252621730b858b6023d9db93
Daniel Elebash <delebash@users.noreply.github.com>
2022-12-11 13:18:45 -0500

    fix xy scale

---
+++ dist/spa/assets/IndexPage.fc62b27f.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.711cc840.js";import{aj as a,aY as t,ak as n,aU as o,an as s,ap as r}from"./index.faa45400.js";const c=t({name:"IndexPage"}),l=r("img",{alt:"Unreal Vault Organizer logo",style:{width:"200px",height:"200px"}},null,-1);function p(i,d,f,_,x,m){return n(),o(e,{class:"flex flex-center"},{default:s(()=>[l]),_:1})}var h=a(c,[["render",p]]);export{h as default};
