0b414a587e4848a9f02d0ebe87bee804631a40a6
Daniel Elebash <delebash@users.noreply.github.com>
2023-04-04 21:55:53 -0400

    help

--- dist/spa/assets/IndexPage.cecc6cde.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.281a2bda.js";import{aM as a,b6 as t,aN as o,b0 as s,aQ as n,aR as r}from"./index.9af64841.js";const c=t({name:"IndexPage"}),l=r("img",{alt:"Unreal Vault Organizer logo",style:{width:"200px",height:"200px"}},null,-1);function i(p,d,f,_,x,m){return o(),s(e,{class:"flex flex-center"},{default:n(()=>[l]),_:1})}var h=a(c,[["render",i]]);export{h as default};
